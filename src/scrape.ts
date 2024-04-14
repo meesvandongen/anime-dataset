1387fd0748a136fe4c36c3b8a7c0e972f4c85b40
Mees van Dongen <meesvandongen@gmail.com>
2024-04-14 16:25:59 +0200

    remove popularity

--- src/scrape.ts
+++ src/scrape.ts
@@ -45,7 +45,7 @@ yearLoop: while (year < lastYearToScrape) {
         offset,
         nsfw: true,
         fields:
-          "id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics",
+          "id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics",
       }).catch((err) => {
         if (err instanceof ApiError) {
           if (err.status === 404) {
