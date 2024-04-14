1a6af2a6b1f44798cae85ad78c7ee6b9fe8fbeab
Mees van Dongen <meesvandongen@gmail.com>
2024-04-14 16:43:37 +0200

    separate genres, studios to ids

--- src/csv.ts
+++ src/csv.ts
@@ -7,39 +7,107 @@ const sortedFilenames = fileNames.toSorted(
   (a, b) => parseInt(a.split(".")[0]) - parseInt(b.split(".")[0]),
 );
 
-const data = sortedFilenames.map((fileName) => {
-  const anime = JSON.parse(
-    fs.readFileSync(`./data/anime/${fileName}`, "utf-8"),
-  );
-
-  if (!anime) {
-    console.error(`Failed to parse ${fileName}`);
-  }
-
-  return {
-    id: anime.id,
-    title: anime.title,
-    titleJa: anime.alternative_titles?.ja,
-    titleEn: anime.alternative_titles?.en,
-    image: anime.main_picture?.medium
-      .replace("https://cdn.myanimelist.net/images/anime/", "")
-      .replace(".jpg", ""),
-    mean: anime.mean,
-    num_list_users: anime.num_list_users,
-    num_scoring_users: anime.num_scoring_users,
-    num_episodes: anime.num_episodes,
-    start_date: anime.start_date,
-    end_date: anime.end_date,
-    media_type: anime.media_type,
-    status: anime.status,
-    rating: anime.rating,
-    average_episode_duration: anime.average_episode_duration,
-    genres: (anime.genres ?? []).map((genre: any) => genre.name).join(", "),
-    studios: (anime.studios ?? []).map((studio: any) => studio.name).join(", "),
-  };
+const allAnimeJsonData = sortedFilenames.map((fileName) => {
+  return JSON.parse(fs.readFileSync(`./data/anime/${fileName}`, "utf-8"));
 });
+{
+  const allAnimeCsvData = allAnimeJsonData.map((singleAnime) => {
+    return {
+      id: singleAnime.id,
+      title: singleAnime.title,
+      titleJa: singleAnime.alternative_titles?.ja,
+      titleEn: singleAnime.alternative_titles?.en,
+      image: singleAnime.main_picture?.medium
+        .replace("https://cdn.myanimelist.net/images/anime/", "")
+        .replace(".jpg", ""),
+      mean: singleAnime.mean,
+      num_list_users: singleAnime.num_list_users,
+      num_scoring_users: singleAnime.num_scoring_users,
+      num_episodes: singleAnime.num_episodes,
+      start_date: singleAnime.start_date,
+      end_date: singleAnime.end_date,
+      media_type: singleAnime.media_type,
+      status: singleAnime.status,
+      rating: singleAnime.rating,
+      average_episode_duration: singleAnime.average_episode_duration,
+      genres: (singleAnime.genres ?? [])
+        .map((genre: any) => genre.id)
+        .join(";"),
+      studios: (singleAnime.studios ?? [])
+        .map((studio: any) => studio.id)
+        .join(";"),
+    };
+  });
 
-stringify(data, { header: true }, (err, output) => {
-  if (err) throw err;
-  fs.writeFileSync("./data/anime.csv", output);
-});
+  stringify(allAnimeCsvData, { header: true }, (err, output) => {
+    if (err) throw err;
+    fs.writeFileSync("./data/anime.csv", output);
+  });
+}
+{
+  const allGenres: any[] = [];
+  allAnimeJsonData.forEach((anime) => {
+    anime.genres?.forEach((genre: any) => {
+      if (!allGenres.find((g) => g.id === genre.id)) {
+        allGenres.push(genre);
+      }
+    });
+  });
+
+  const sortedGenres = allGenres.toSorted((a, b) => a.id - b.id);
+
+  stringify(sortedGenres, { header: true }, (err, output) => {
+    if (err) throw err;
+    fs.writeFileSync("./data/genres.csv", output);
+  });
+}
+{
+  const allStudios: any[] = [];
+  allAnimeJsonData.forEach((anime) => {
+    anime.studios?.forEach((studio: any) => {
+      if (!allStudios.find((s) => s.id === studio.id)) {
+        allStudios.push(studio);
+      }
+    });
+  });
+
+  const sortedStudios = allStudios.toSorted((a, b) => a.id - b.id);
+
+  stringify(sortedStudios, { header: true }, (err, output) => {
+    if (err) throw err;
+    fs.writeFileSync("./data/studios.csv", output);
+  });
+}
+
+{
+  const allAnimedataStandalone = allAnimeJsonData.map((singleAnime) => {
+    return {
+      id: singleAnime.id,
+      title: singleAnime.title,
+      titleJa: singleAnime.alternative_titles?.ja,
+      titleEn: singleAnime.alternative_titles?.en,
+      image: singleAnime.main_picture?.medium,
+      mean: singleAnime.mean,
+      num_list_users: singleAnime.num_list_users,
+      num_scoring_users: singleAnime.num_scoring_users,
+      num_episodes: singleAnime.num_episodes,
+      start_date: singleAnime.start_date,
+      end_date: singleAnime.end_date,
+      media_type: singleAnime.media_type,
+      status: singleAnime.status,
+      rating: singleAnime.rating,
+      average_episode_duration: singleAnime.average_episode_duration,
+      genres: (singleAnime.genres ?? [])
+        .map((genre: any) => genre.name)
+        .join(", "),
+      studios: (singleAnime.studios ?? [])
+        .map((studio: any) => studio.name)
+        .join(", "),
+    };
+  });
+
+  stringify(allAnimedataStandalone, { header: true }, (err, output) => {
+    if (err) throw err;
+    fs.writeFileSync("./data/anime-standalone.csv", output);
+  });
+}
